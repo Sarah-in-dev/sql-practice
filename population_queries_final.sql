653a40c15c681ad17df0be80ac1199dd44208973
Sarah-in-dev <50673314+Sarah-in-dev@users.noreply.github.com>
2019-06-18 16:38:32 -0400

    Add files via upload

--- population_queries_final.sql
+++ population_queries_final.sql
@@ -1,34 +1,47 @@
--- This is the first query:
+-- How many entries in the database are from Africa?
 
-SELECT DISTINCT year FROM population_years;
+SELECT COUNT(*) from countries where
+continent = 'Africa';
 
--- Populations of Gabon:
+-- What was the total population of Oceania in 2005?
 
-SELECT * FROM population_years
-WHERE country = 'Gabon';
-
--- Smallest 2005 countries by population:
-
-SELECT country FROM population_years
+SELECT SUM(population) FROM population_years
+INNER JOIN countries ON
+countries.id = population_years.country_id
 WHERE year = 2005
-ORDER BY population ASC
-LIMIT 10;
+AND continent = 'Oceania';
 
--- Countries with >100M population in 2010
+-- What is the average population of countries in South America in 2003?
 
-SELECT DISTINCT country FROM population_years
+SELECT AVG(population) FROM population_years
+INNER JOIN countries ON
+countries.id = population_years.country_id
+WHERE year = 2003
+AND continent = 'South America';
+
+-- What country had the smallest population in 2007?
+
+SELECT MIN(population), name FROM population_years
+INNER JOIN countries ON
+countries.id = population_years.country_id
+WHERE year = 2007;
+
+-- What is the average population of Poland during the time period covered by this dataset?
+
+SELECT AVG(population), name FROM population_years
+INNER JOIN countries ON
+countries.id = population_years.country_id
+WHERE name = 'Poland';
+
+-- How many countries have the word "The" in their name?
+
+SELECT COUNT(*) FROM countries
+where name LIKE "%The%";
+
+-- What was the total population of each continent in 2010?
+
+SELECT SUM(population), continent FROM population_years
+INNER JOIN countries ON
+countries.id = population_years.country_id
 WHERE year = 2010
-AND population > 100;
-
--- Countries containing "islands"
-
-SELECT DISTINCT country FROM population_years
-WHERE country like "%islands%";
-
--- Difference in population in Indonesia:
-
-SELECT year, population FROM population_years
-WHERE country = 'Indonesia'
-AND year = 2000
-OR country = 'Indonesia'
-AND year = 2010;
+GROUP BY continent;
